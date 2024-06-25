29ebf7da41cd1556c6add4e326a0907e907e06c7
Lucy Moore <lucy.moore@devacademy.co.nz>
2024-06-25 12:27:02 +1200

    setting up client side and installing modemon

---
+++ client/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Column from './Column'
+import Task from './Task'
+function App() {
+  return (
+    <>
+      <h1>APP</h1>
+    </>
+  )
+}
+export default App
---
+++ client/components/Column.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Column() {
+  return (
+    <>
+      <h1>Column</h1>
+    </>
+  )
+}
+export default Column
